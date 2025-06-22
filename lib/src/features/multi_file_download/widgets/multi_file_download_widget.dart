a1897cf1aae010a977d3b73d4115c03f534ced75
sb-dor <avaz.shams.2002@gmail.com>
2025-06-22 17:17:56 +0500

    made changes

--- lib/src/features/multi_file_download/widgets/multi_file_download_widget.dart
+++ lib/src/features/multi_file_download/widgets/multi_file_download_widget.dart
@@ -10,11 +10,24 @@ class MultiFileDownloadWidget extends StatefulWidget {
 class _MultiFileDownloadWidgetState extends State<MultiFileDownloadWidget> {
   late final MultiFileDownloaderController _multiFileDownloaderController;
 
+  late final List<String> mediaUrls;
+
   @override
   void initState() {
     super.initState();
     _multiFileDownloaderController =
         MultifileDownloadConfigInhWidget.of(context).multiFileDownloaderController;
+
+    mediaUrls = [
+      '/id/1/1920/1010.jpg',
+      '/id/2/1920/1010.jpg',
+      '/id/3/1920/1010.jpg',
+      '/id/4/1920/1010.jpg',
+      '/id/5/1920/1010.jpg',
+      '/id/5/1920/1010.jpg',
+      '/id/6/1920/1010.jpg',
+      '/id/7/1920/1010.jpg',
+    ];
   }
 
   @override
